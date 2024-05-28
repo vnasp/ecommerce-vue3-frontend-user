dddadc86bbf65ea2c0b078a4a299acc1f799cd3c
yesminalina <yesmin.pizarro@gmail.com>
2024-05-27 23:17:00 -0400

    Se define estilo de display flex de footer

--- src/components/SiteFooter/FeaturesSection.vue
+++ src/components/SiteFooter/FeaturesSection.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-sección de footer - características
-  </div>
+  <section class="flex justify-evenly py-8">
+    <section class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 w-5/6 border-b border-indigo-400 pb-4">
+      <article>Feature 1</article>
+      <article>Feature 2</article>
+      <article>Feature 3</article>
+      <article>Feature 4</article>
+      <article>Feature 5</article>
+    </section>
+  </section>
 </template>
 
 <script>
