ec0b4224211a4308a4d22f7a96dd79e3ca985952
yesminalina <yesmin.pizarro@gmail.com>
2024-05-29 23:21:53 -0400

    Se agrega estilo a cards de componente featureSection del footer

--- src/main.js
+++ src/main.js
@@ -4,6 +4,25 @@ import './assets/tailwind.css'
 
 Vue.config.productionTip = false
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faSatelliteDish } from '@fortawesome/free-solid-svg-icons'
+import { faWallet } from '@fortawesome/free-solid-svg-icons'
+import { faPercent } from '@fortawesome/free-solid-svg-icons'
+import { faComment } from '@fortawesome/free-solid-svg-icons'
+import { faListCheck } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faSatelliteDish, faWallet, faPercent, faComment, faListCheck)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
