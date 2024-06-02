9ab4dded479b5074cbe26e1bba2e3a2da3eacffc
Valentina Muñoz <valentinamr@gmail.com>
2024-06-01 20:47:26 -0400

    Update

--- src/components/SiteFooter/FooterSection.vue
+++ src/components/SiteFooter/FooterSection.vue
@@ -1,22 +1,19 @@
 <template>
-  <div>
+  <div class="bg-indigo-900 text-white">
     <FeaturesSection />
     <LinksSection />
-    <NewsletterSection />
   </div>
 </template>
 
 <script>
 import FeaturesSection from './FeaturesSection.vue';
 import LinksSection from './LinksSection.vue';
-import NewsletterSection from './NewsletterSection.vue';
 
 export default {
   name: 'FooterSection',
   components: {
     FeaturesSection,
-    LinksSection,
-    NewsletterSection
+    LinksSection
   }
 }
 </script>
