75d7ba02e037b39655abfaf258cf1229aac21aad
Valentina Muñoz <valentinamr@gmail.com>
2024-06-08 19:12:33 -0400

    More icons

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific solid icons */
-import { faSatelliteDish, faWallet, faPercent, faComment, faListCheck, faBagShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faSatelliteDish, faWallet, faPercent, faComment, faListCheck, faBagShopping, faMagnifyingGlass, faChevronRight, faChevronLeft, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 /* import specific regular icons */
 import { faUser, faHeart } from '@fortawesome/free-regular-svg-icons'
@@ -20,7 +20,7 @@ import { faUser, faHeart } from '@fortawesome/free-regular-svg-icons'
 import { faFacebookF, faTwitter, faYoutube, faPinterestP, faSkype } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faSatelliteDish, faWallet, faPercent, faComment, faListCheck, faUser, faHeart, faBagShopping, faMagnifyingGlass, faFacebookF, faTwitter, faYoutube, faPinterestP, faSkype)
+library.add(faSatelliteDish, faWallet, faPercent, faComment, faListCheck, faUser, faHeart, faBagShopping, faMagnifyingGlass, faFacebookF, faTwitter, faYoutube, faPinterestP, faSkype,faChevronRight, faChevronLeft, faPlus)
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
