793e7860f3f653845946248d62e18215321494a8
yesminalina <yesmin.pizarro@gmail.com>
2024-06-14 23:25:43 -0400

    Adds Newsletter form

--- src/components/SiteFooter/FeaturesSection.vue
+++ src/components/SiteFooter/FeaturesSection.vue
@@ -1,30 +1,30 @@
 <template>
   <section class="flex justify-center py-8">
-    <section class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-10 border-b border-indigo-400 pb-4 text-center text-xs">
+    <section class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-10 border-b border-indigo-400 pb-4 text-center">
       <article class="p-4">
         <font-awesome-icon icon="fa-solid fa-satellite-dish" size="lg" class="mb-1"/>
-        <h3 class="h3-feat-card">FAST DELIVERY</h3>
-        <p class="p-feat-card">Across West & East India</p>
+        <h3 class="h3-feat-card font-bold uppercase text-xs">Fast Delivery</h3>
+        <p class="p-feat-card text-sm">Across West & East India</p>
       </article>
       <article class="p-4">
         <font-awesome-icon icon="fa-solid fa-wallet" size="lg" class="mb-1" />
-        <h3 class="h3-feat-card">SAFE PAYMENT</h3>
-        <p class="p-feat-card">100% Secure Payment</p>
+        <h3 class="h3-feat-card font-bold uppercase text-xs">Safe Payment</h3>
+        <p class="p-feat-card text-sm">100% Secure Payment</p>
       </article>
       <article class="p-4">
         <font-awesome-icon icon="fa-solid fa-percent" size="lg" class="mb-1" />
-        <h3 class="h3-feat-card">ONLINE DISCOUNT</h3>
-        <p class="p-feat-card">Add Multi-buy Discount</p>
+        <h3 class="h3-feat-card font-bold uppercase text-xs">Online Discount</h3>
+        <p class="p-feat-card text-sm">Add Multi-buy Discount</p>
       </article>
       <article class="p-4">
         <font-awesome-icon icon="fa-solid fa-comment" size="lg" class="mb-1" />
-        <h3 class="h3-feat-card">HELP CENTER</h3>
-        <p class="p-feat-card">Dedicated 24/7 Support</p>
+        <h3 class="h3-feat-card font-bold uppercase text-xs">Help Center</h3>
+        <p class="p-feat-card text-sm">Dedicated 24/7 Support</p>
       </article>
       <article class="p-4">
         <font-awesome-icon icon="fa-solid fa-list-check" size="lg" class="mb-1" />
-        <h3 class="h3-feat-card">CURATED ITEMS</h3>
-        <p class="p-feat-card">From Handpicked Sellers</p>
+        <h3 class="h3-feat-card font-bold uppercase text-xs">Curated Items</h3>
+        <p class="p-feat-card text-sm">From Handpicked Sellers</p>
       </article>
     </section>
   </section>
--- src/components/SiteFooter/LinksSection.vue
+++ src/components/SiteFooter/LinksSection.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="flex flex-col lg:flex-row flex-wrap justify-evenly pb-6 px-20">
-    <section class="flex flex-col md:flex-row flex-none md:1/3 lg:w-8/12 justify-center text-sm">
+    <section class="flex flex-col md:flex-row flex-none md:1/3 lg:w-8/12 justify-center text-base">
       <article class="flex-auto md:w-4/12 pe-3 mb-10">
-        <h4 class="uppercase mb-5">let us help you</h4>
+        <h4 class="uppercase mb-5 text-sm font-bold"><Link:mf></Link:mf>Let us help you</h4>
         <p class="mb-5 text-indigo-200">If you have any question, please contact us at: <span class="text-lime-600">support@example.com</span></p>
         <div>
           <p class="text-indigo-200 mb-1">Social Media:</p>
@@ -14,7 +14,7 @@
         </div>
       </article>
       <article class="flex-auto md:w-5/12 pe-3 mb-10">
-        <h4 class="uppercase mb-5">looking for orfarm?</h4>
+        <h4 class="uppercase mb-5 text-sm font-bold">Looking for orfarm?</h4>
         <p class="mb-5 text-indigo-200">68 St. Vicent Place, Glasgow, Greater Newyork NH2012, UK.</p>
         <div class="text-indigo-200">
           <p>Monday – Friday: <span class="text-white">8:10 AM – 6:10 PM</span></p>
@@ -23,7 +23,7 @@
         </div>
       </article>
       <article class="flex-auto md:w-3/12 pe-3 mb-10">
-        <h4 class="uppercase mb-5">hot categories</h4>
+        <h4 class="uppercase mb-5 text-sm font-bold">Hot categories</h4>
         <div>
           <ul>
             <li class="mb-2 transition ease-in-out duration-600 hover:translate-x-2"><a class="text-indigo-200 hover:text-white hover:cursor-pointer">Fruits & Vegetables</a></li>
@@ -35,7 +35,7 @@
         </div>
       </article>
     </section>
-    <NewsletterSection class="flex-none md:w-1/3 lg:w-4/12"/>
+    <NewsletterSection class="flex-auto md:w-1/2 md:text-left lg:w-4/12"/>
   </section>
 </template>
 
