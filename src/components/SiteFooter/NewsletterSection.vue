84d2d6088d3ff4a8494915009146463d44e24b51
yesminalina <yesmin.pizarro@gmail.com>
2024-06-14 23:32:12 -0400

    Changes button type on html

--- src/components/SiteFooter/NewsletterSection.vue
+++ src/components/SiteFooter/NewsletterSection.vue
@@ -6,7 +6,7 @@
       <div class="flex items-center w-full rounded bg-white">
         <font-awesome-icon icon="fa-regular fa-envelope" class="text-gray-400 ms-4" size="lg" />
         <input class="w-full rounded border-none ps-2 py-1 outline-none text-gray-900 text-sm" placeholder="Your email adress..." type="text">
-        <button class="m-1 rounded px-4 py-3 uppercase font-bold bg-lime-600 text-center text-xs">Subscribe</button>
+        <button class="m-1 rounded px-4 py-3 uppercase font-bold bg-lime-600 text-center text-xs" type="submit">Subscribe</button>
       </div>
       <div class="flex items-center mt-3">
         <input type="checkbox">
